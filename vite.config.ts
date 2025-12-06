dc406dd3bc4c88af9ba688c962dbf9f75f6c8da9
vladimir jara benites <u202022365@upc.edu.pe>
2025-12-05 21:54:34 -0500

    fix: error

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  base: process.env.NODE_ENV === "production" ? "/web-gases-nobles/" : "/",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
